329139a7edb188e75461fb791b409e3cb09c5f2a
settyan117 <sute-kidaisuki@hotmail.co.jp>
2022-05-30 17:04:08 +0900

    単語リストを変更できるように

--- SoundTyping/MainForm.Designer.cs
+++ SoundTyping/MainForm.Designer.cs
@@ -34,15 +34,22 @@
             this.labelClearCountValue = new System.Windows.Forms.Label();
             this.labelMissCountTitle = new System.Windows.Forms.Label();
             this.labelClearCountTitle = new System.Windows.Forms.Label();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.changeWords = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeScore = new System.Windows.Forms.ToolStripMenuItem();
+            this.wordsFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.scoreFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.panelInform.SuspendLayout();
+            this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelSample
             // 
             this.panelSample.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelSample.Location = new System.Drawing.Point(145, 204);
+            this.panelSample.Location = new System.Drawing.Point(127, 153);
+            this.panelSample.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelSample.Name = "panelSample";
-            this.panelSample.Size = new System.Drawing.Size(770, 70);
+            this.panelSample.Size = new System.Drawing.Size(665, 55);
             this.panelSample.TabIndex = 0;
             // 
             // panelInform
@@ -51,17 +58,18 @@
             this.panelInform.Controls.Add(this.labelClearCountValue);
             this.panelInform.Controls.Add(this.labelMissCountTitle);
             this.panelInform.Controls.Add(this.labelClearCountTitle);
-            this.panelInform.Location = new System.Drawing.Point(940, 193);
+            this.panelInform.Location = new System.Drawing.Point(822, 145);
+            this.panelInform.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelInform.Name = "panelInform";
-            this.panelInform.Size = new System.Drawing.Size(270, 81);
+            this.panelInform.Size = new System.Drawing.Size(236, 61);
             this.panelInform.TabIndex = 1;
             // 
             // labelMissCountValue
             // 
             this.labelMissCountValue.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelMissCountValue.Location = new System.Drawing.Point(163, 39);
+            this.labelMissCountValue.Location = new System.Drawing.Point(143, 29);
             this.labelMissCountValue.Name = "labelMissCountValue";
-            this.labelMissCountValue.Size = new System.Drawing.Size(107, 39);
+            this.labelMissCountValue.Size = new System.Drawing.Size(94, 29);
             this.labelMissCountValue.TabIndex = 3;
             this.labelMissCountValue.Text = "0";
             this.labelMissCountValue.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -69,9 +77,9 @@
             // labelClearCountValue
             // 
             this.labelClearCountValue.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelClearCountValue.Location = new System.Drawing.Point(163, 0);
+            this.labelClearCountValue.Location = new System.Drawing.Point(143, 0);
             this.labelClearCountValue.Name = "labelClearCountValue";
-            this.labelClearCountValue.Size = new System.Drawing.Size(107, 39);
+            this.labelClearCountValue.Size = new System.Drawing.Size(94, 29);
             this.labelClearCountValue.TabIndex = 2;
             this.labelClearCountValue.Text = "0";
             this.labelClearCountValue.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -79,9 +87,9 @@
             // labelMissCountTitle
             // 
             this.labelMissCountTitle.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelMissCountTitle.Location = new System.Drawing.Point(0, 39);
+            this.labelMissCountTitle.Location = new System.Drawing.Point(0, 29);
             this.labelMissCountTitle.Name = "labelMissCountTitle";
-            this.labelMissCountTitle.Size = new System.Drawing.Size(146, 39);
+            this.labelMissCountTitle.Size = new System.Drawing.Size(128, 29);
             this.labelMissCountTitle.TabIndex = 1;
             this.labelMissCountTitle.Text = "ミスタイプ";
             this.labelMissCountTitle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -91,26 +99,56 @@
             this.labelClearCountTitle.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.labelClearCountTitle.Location = new System.Drawing.Point(0, 0);
             this.labelClearCountTitle.Name = "labelClearCountTitle";
-            this.labelClearCountTitle.Size = new System.Drawing.Size(146, 39);
+            this.labelClearCountTitle.Size = new System.Drawing.Size(128, 29);
             this.labelClearCountTitle.TabIndex = 0;
             this.labelClearCountTitle.Text = "クリア";
             this.labelClearCountTitle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeWords,
+            this.changeScore});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1104, 24);
+            this.menuStrip.TabIndex = 2;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // changeWords
+            // 
+            this.changeWords.Name = "changeWords";
+            this.changeWords.Size = new System.Drawing.Size(110, 20);
+            this.changeWords.Text = "単語帳を変更 (&W)";
+            this.changeWords.Click += new System.EventHandler(this.changeWords_Click);
+            // 
+            // changeScore
+            // 
+            this.changeScore.Name = "changeScore";
+            this.changeScore.Size = new System.Drawing.Size(93, 20);
+            this.changeScore.Text = "楽譜を変更 (&S)";
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1262, 670);
+            this.ClientSize = new System.Drawing.Size(1104, 502);
             this.Controls.Add(this.panelInform);
             this.Controls.Add(this.panelSample);
+            this.Controls.Add(this.menuStrip);
             this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainForm_KeyPress);
             this.panelInform.ResumeLayout(false);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -122,5 +160,10 @@
         private Label labelClearCountValue;
         private Label labelMissCountTitle;
         private Label labelClearCountTitle;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem changeWords;
+        private ToolStripMenuItem changeScore;
+        private OpenFileDialog wordsFileDialog;
+        private OpenFileDialog scoreFileDialog;
     }
 }
--- SoundTyping/MainForm.cs
+++ SoundTyping/MainForm.cs
@@ -1,13 +1,16 @@
 ﻿using NextMidi.MidiPort.Output.Core;
 using NextMidi.MidiPort.Output;
 using NextMidi.DataElement;
+using System.Text.RegularExpressions;
 
 namespace SoundTyping
 {
     public partial class MainForm : Form
     {
+        const string charsAvailable = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!;:,.? ";
+
         private LabelArray sampleChars;
-        private string[] answers = new string[] {"I believe.", "Hello, World!"};
+        private List<string> words = new() { "I believe.", "Hello, World!" };
         private int rightCount = 0;
         private int missCountSum = 0;
         private int clearCountSum = 0;
@@ -15,12 +18,15 @@ namespace SoundTyping
 
         private MidiOutPort? port;
         private int noteCount = 0;
-        private byte[] score = { 59, 55, 57, 59, 62, 60, 60, 64, 62, 62, 67, 66, 67, 62, 59, 55, 57, 59, 60, 62, 64, 62, 60, 59, 57, 59, 55, 54, 55, 57, 50, 54, 57, 60, 59, 57 };
+        // private byte[] score = { 59, 55, 57, 59, 62, 60, 60, 64, 62, 62, 67, 66, 67, 62, 59, 55, 57, 59, 60, 62, 64, 62, 60, 59, 57, 59, 55, 54, 55, 57, 50, 54, 57, 60, 59, 57 };
+        // private byte[] score = { 71, 67, 69, 71, 74, 72, 72, 76, 74, 74, 79, 78, 79, 74, 71, 67, 69, 71, 72, 74, 76, 74, 72, 71, 69, 71, 67, 66, 67, 69, 62, 66, 69, 72, 71, 69, 71, 67, 69, 71, 74, 72, 72, 76, 74, 74, 79, 78, 79, 74, 71, 67, 69, 71, 64, 74, 72, 71, 69, 67, 62, 67, 66, 67, 71, 74, 79, 74, 71, 67, 71, 73, 74, 62, 64, 66, 69, 68, 68, 71, 69, 69, 72, 71, 72, 69, 64, 69, 62, 64, 65, 74, 72, 74, 71, 68, 64, 66, 68, 69, 72, 71, 72, 76, 74, 74, 77, 76, 76, 81, 80, 81, 76, 72, 69, 71, 72, 77, 76, 74, 72, 71, 69, 64, 69, 68, 69, 197, 197, 197, 197, 197, 197, 197, 197 }; // 主よ人の望みの喜びよ
+        private byte[] score = {68, 68, 69, 68, 67, 68, 73, 76, 75, 73, 75, 73, 72, 73, 76, 80, 80, 68, 69, 68, 67, 68, 73, 76, 75, 73, 75, 73, 72, 73, 76, 80, 80, 69, 73, 75, 78, 81, 85, 87, 95, 93, 92, 90, 88, 87, 90, 85, 84, 87, 81, 80, 78, 81, 76, 75, 78, 73, 72, 75, 69, 68, 71, 69, 69, 68, 69, 68, 67, 68, 73, 76, 75, 73, 75, 73, 72, 73, 76, 80, 80, 68, 70, 68, 67, 68, 73, 76, 75, 73, 75, 73, 72, 73, 76, 80, 75, 76, 75, 74, 75, 83, 82, 80, 79, 88, 87, 85, 83, 82, 80, 79, 82, 80, 83, 74, 76, 75, 80, 70, 73, 71, 75, 67, 70, 68, 67, 68, 68, 80, 72, 73, 66, 78, 72, 73, 65, 77, 72, 73, 66, 78, 72, 73, 61, 73, 66, 69, 63, 75, 66, 69, 64, 76, 68, 71, 68, 80, 71, 76, 68, 80, 72, 73, 66, 78, 72, 73, 65, 77, 72, 73, 66, 78, 72, 73, 65, 77, 71, 75, 66, 78, 71, 75, 69, 81, 71, 76, 68, 80, 71, 76, 68, 80, 72, 73, 66, 78, 72, 73, 65, 77, 72, 73, 66, 78, 72, 73, 61, 73, 66, 69, 63, 75, 66, 69, 64, 76, 68, 71, 68, 80, 71, 76, 68, 80, 72, 73, 66, 78, 72, 73, 75, 87, 78, 81, 73, 85, 78,
+81, 73, 85, 75, 78, 72, 84, 75, 78, 72, 84, 75, 78, 72, 84, 75, 78, 72, 84, 75, 78, 71, 83, 75, 78, 71, 83, 75, 78, 70, 82, 75, 78, 70, 82, 75, 78, 69, 81, 75, 78, 69, 81, 75, 78, 69, 81, 75, 78, 72, 84, 75, 78, 71, 83, 75, 78, 71, 83, 75, 78, 70, 82, 75, 78, 70, 82, 75, 78, 69, 81, 75, 78, 69, 81, 75, 78, 68, 80, 75, 78 }; // 幻想即興曲
 
         public MainForm()
         {
             InitializeComponent();
-            sampleChars = new LabelArray(22, new Size(35, 70), this.panelSample);
+            sampleChars = new LabelArray(22, new Size(35, 55), this.panelSample);
         }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -28,7 +34,7 @@ namespace SoundTyping
             port = new MidiOutPort("Microsoft GS Wavetable Synth");
             port.Open();
 
-            answer = answers[0];
+            answer = words[0];
             sampleChars.Text = answer;
         }
 
@@ -43,6 +49,7 @@ namespace SoundTyping
             }
             else
             {
+                Task.Run(() => { Console.Beep(800, 50); });
                 missCountSum++;
                 labelMissCountValue.Text = missCountSum.ToString();
             }
@@ -60,7 +67,7 @@ namespace SoundTyping
             sampleChars.SetForeColorAll(SystemColors.ControlText);
             sampleChars.SetBackColorAll(SystemColors.Window);
             Random rng = new System.Random();
-            answer = answers[rng.Next(0, 2)];
+            answer = words[rng.Next(0, words.Count)];
             rightCount = 0;
             sampleChars.Text = answer;
         }
@@ -68,12 +75,38 @@ namespace SoundTyping
         private void PlayNext()
         {
             if (port == null) throw new Exception();
-            byte nprev = score[(noteCount + score.Length - 1) % score.Length];
+            byte nprev = (byte)(score[(noteCount + score.Length - 1) % score.Length] % 128);
             byte n = score[noteCount];
-            port.Send(new NoteOffEvent(nprev));
-            port.Send(new NoteOnEvent(n, 112));
+            if (n < 128)
+            {
+                port.Send(new NoteOffEvent(nprev));
+                port.Send(new NoteOnEvent(n, 112));
+            }
             noteCount++;
             noteCount %= score.Length;
+        }
+
+        private void changeWords_Click(object sender, EventArgs e)
+        {
+            if (wordsFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                List<string> wordsNew = new();
+                StreamReader sr = new StreamReader(wordsFileDialog.FileName);
+                while (!sr.EndOfStream)
+                {
+                    string? word = sr.ReadLine();
+                    if (word != null && word.Length <= 19 && Regex.IsMatch(word, @"[A-Za-z0-9,.!? ]+"))
+                    {
+                        wordsNew.Add(word);
+                    }
+                }
+
+                if (wordsNew.Count > 0)
+                {
+                    words = wordsNew;
+                    ResetGame();
+                }
+            }
         }
     }
 }
